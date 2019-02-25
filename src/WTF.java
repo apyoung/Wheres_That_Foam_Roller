0e4a79219134a9d41e8548f4ffb2405c74fc1ede
Anton Neuhold Jr <agneuhold@gmail.com>
2019-02-24 17:02:23 -0800

    Remove splashscreen Removed splashscreen because of compilation issues.

--- src/WTF.java
+++ src/WTF.java
@@ -1,5 +1,4 @@
 import gui.DatabaseConnection;
-import gui.SplashScreen;
 
 import javax.swing.*;
 
@@ -10,25 +9,7 @@ public class WTF {
         } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
             e.printStackTrace();
         }
-
-        SplashScreen splashScreen = new SplashScreen();
-        splashScreen.setVisible(true);
-        try {
-            for (int i = 0; i <= 100; i++) {
-                Thread.sleep(20);
-                splashScreen.LoadingProgressLabel.setText("Loading " + i + "%");
-                splashScreen.progressBar.setValue(i);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        splashScreen.dispose();
-
-        SwingUtilities.invokeLater(new Runnable() {
-            @Override public void run() {
-                DatabaseConnection dbc = new DatabaseConnection();
-                dbc.setVisible(true);
-            }
-        });
+        DatabaseConnection dbc = new DatabaseConnection();
+        dbc.setVisible(true);
     }
 }
